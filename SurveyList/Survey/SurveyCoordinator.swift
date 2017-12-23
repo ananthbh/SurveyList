1756662455495df06abeb2d5b7a82dc60ed38ca0
ananthbh6 <ananth@chestnote.com>
2017-12-23 20:56:56 +0100

    created survey coordinator

---
+++ SurveyList/Survey/SurveyCoordinator.swift
@@ -0,0 +1,20 @@
+//
+//  SurveyCoordinator.swift
+//  SurveyList
+//
+//  Created by Ananth Bhamidipati on 23/12/17.
+//  Copyright © 2017 Ananth Bhamidipati. All rights reserved.
+//
+
+import Foundation
+
+final class SurveyCoordinator {
+    
+    private let networkProvider: NetworkProvider!
+    
+    init(networkProvider: NetworkProvider) {
+        self.networkProvider = networkProvider
+    }
+    
+    
+}
