8185302a6616d77d7c46595d365498b752be99f4
ananthbh6 <ananth@chestnote.com>
2017-12-24 17:24:24 +0100

    added surveyPageViewModel and fetching the surveys and displaying them

--- SurveyList/Survey/ViewController/SurveyPageViewController.swift
+++ SurveyList/Survey/ViewController/SurveyPageViewController.swift
@@ -8,12 +8,32 @@
 
 import UIKit
 
+protocol PageContentViewController {
+    var pageIndex: Int { get set }
+}
+
 class SurveyPageViewController: UIPageViewController {
 
+    private var viewModel: SurveyPageViewModel!
+    private var currentPage = 0
+    
+    convenience init(viewModel:SurveyPageViewModel) {
+        self.init(transitionStyle: .scroll,
+                  navigationOrientation: .vertical,
+                  options: nil)
+        self.viewModel = viewModel
+        dataSource = self
+        delegate = self
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        viewModel.fetchSurveys {
+            let controller = self.viewControllerAtIndex(self.currentPage)
+            self.setViewControllers([controller], direction: UIPageViewControllerNavigationDirection.forward, animated: true, completion: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +41,95 @@ class SurveyPageViewController: UIPageViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func surveyViewController(_ viewModel: SurveyViewModel) -> SurveyViewController {
+        weak var weak = self
+        
+        let transitions = SurveyViewTransitions()
+        let surveyViewController = SurveyViewController(viewModel: viewModel, transitions: transitions)
+        return surveyViewController
     }
-    */
+    
+    func viewControllerAtIndex(_ index:Int) -> UIViewController {
+        let surveyViewModel = self.viewModel.viewModel(at: index)
+        let controller = surveyViewController(surveyViewModel)
+        return controller
+    }
+    
+    private func swipeToNextPreviousSurveyOrClose() {
+        var shouldDismiss = false
+        var direction:UIPageViewControllerNavigationDirection = .forward
+        if currentPage < viewModel.numberOfSurveys {
+            direction = .forward
+        } else {
+            if viewModel.numberOfSurveys > 0 {
+                currentPage = currentPage - 1
+                direction = .reverse
+            } else {
+                shouldDismiss = true
+            }
+        }
+        if shouldDismiss {
+        //    transitions.onClose()
+        } else {
+            let controller = viewControllerAtIndex(currentPage)
+            setViewControllers([controller],
+                               direction: direction,
+                               animated: true,
+                               completion: nil)
+        }
+    }
 
 }
+
+
+extension SurveyPageViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        let vc = viewController as! PageContentViewController
+        var index = vc.pageIndex
+        if (index == 0 || index == NSNotFound) {
+            return nil
+        }
+        index = index - 1
+        return self.viewControllerAtIndex(index)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        let vc = viewController as! PageContentViewController
+        var index = vc.pageIndex as Int
+        if (index == NSNotFound) {
+            return nil
+        }
+        index = index + 1
+        if (index == viewModel.numberOfSurveys) {
+            return nil
+        }
+        return self.viewControllerAtIndex(index)
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return viewModel.numberOfSurveys
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+    
+}
+
+extension SurveyPageViewController: UIPageViewControllerDelegate {
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            didFinishAnimating finished: Bool,
+                            previousViewControllers: [UIViewController],
+                            transitionCompleted completed: Bool) {
+        if !completed { return }
+        if let controller = pageViewController.viewControllers?.last as? PageContentViewController {
+            currentPage = controller.pageIndex
+            print("currentPage: \(currentPage)")
+        }
+    }
+    
+    
+}
+
--- SurveyList/Survey/ViewModel/SurveyPageViewModel.swift
+++ SurveyList/Survey/ViewModel/SurveyPageViewModel.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+
+protocol SurveyPageViewModel {
+    func viewModel(at index:Int) -> SurveyViewModel
+    var numberOfSurveys: Int { get }
+    func fetchSurveys(completion:@escaping () -> ())
+}
+
+class SurveyPageViewModelImpl: SurveyPageViewModel {
+    
+    let provider: NetworkProvider
+    
+    fileprivate var surveys: [Survey] = []
+    
+    init(provider: NetworkProvider) {
+        self.provider = provider
+    }
+    
+    func fetchSurveys(completion:@escaping () -> ()) {
+        self.provider.request(.surveys) { (result) in
+            switch result {
+            case .success(let response):
+                do {
+                    let surveys = try response.map([Survey].self)
+                    self.surveys = surveys
+                    completion()
+                    print("the surveys are \(surveys.count)")
+                } catch {
+                    print("failed fetching surveys")
+                }
+            case .failure:
+                print("failed fetched surveys")
+            }
+        }
+    }
+    
+    var numberOfSurveys: Int {
+        return surveys.count
+    }
+    
+    func viewModel(at index: Int) -> SurveyViewModel {
+        let survey = surveys[index]
+        let viewModel = SurveyViewModel(survey: survey, provider: self.provider)
+        return viewModel
+    }
+    
+}
