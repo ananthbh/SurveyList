c3557efa87edfe98696b6c81832dec307a3fa9be
ananthbh6 <ananth@chestnote.com>
2017-12-24 16:01:19 +0100

    showing surveyController to start with

--- SurveyList/Survey/SurveyCoordinator.swift
+++ SurveyList/Survey/SurveyCoordinator.swift
@@ -14,9 +14,21 @@ final class SurveyCoordinator {
     private let networkProvider: NetworkProvider!
     private let rootController: UINavigationController!
     
+    var surveyViewModel: SurveyViewModel!
+    
     init(rootController: UINavigationController, networkProvider: NetworkProvider) {
         self.networkProvider = networkProvider
         self.rootController = rootController
+        let surveyController = surveyViewController()
+        rootController.setViewControllers([surveyController], animated: true)
+    }
+    
+    func surveyViewController() -> SurveyViewController {
+        let viewModel = SurveyViewModel(provider: self.networkProvider)
+        let transitions = SurveyViewTransitions()
+        self.surveyViewModel = viewModel
+        let surveyViewController = SurveyViewController(viewModel: viewModel, transitions: transitions)
+        return surveyViewController
     }
     
     
