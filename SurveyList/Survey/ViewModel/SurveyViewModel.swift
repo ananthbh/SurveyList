4fa2169cbe10ee67d5b770a2ff1d125935a43ee1
ananthbh6 <ananth@chestnote.com>
2017-12-24 15:50:39 +0100

    added surveyviewmodel file

---
+++ SurveyList/Survey/ViewModel/SurveyViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SurveyViewModel.swift
+//  SurveyList
+//
+//  Created by Ananth Bhamidipati on 24/12/17.
+//  Copyright © 2017 Ananth Bhamidipati. All rights reserved.
+//
+
+import Foundation
+
+
+final class SurveyViewModel {
+    
+}
