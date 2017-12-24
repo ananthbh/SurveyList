0d273b861963136fcdadf19d71fd55d210931b57
ananthbh6 <ananth@chestnote.com>
2017-12-24 16:43:40 +0100

    created surveypageViewModel

---
+++ SurveyList/Survey/ViewModel/SurveyPageViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SurveyPageViewModel.swift
+//  SurveyList
+//
+//  Created by Ananth Bhamidipati on 24/12/17.
+//  Copyright © 2017 Ananth Bhamidipati. All rights reserved.
+//
+
+import Foundation
